324ed60dbcf3d03f0730f0e694d4d24a6da5415c
SHUU <59950875+jesniey11@users.noreply.github.com>
2023-11-02 23:32:15 +0900

    [Bronze V] Title: 문자열, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

--- 백준/Bronze/9086. 문자열/문자열.cc
+++ 백준/Bronze/9086. 문자열/문자열.cc
@@ -6,25 +6,11 @@ using namespace std;
 int main() {
 	int t;
 	string str;
-	char start, end;
 
 	cin >> t;
 	for (int i = 0; i < t; i++) {
 		cin >> str;
-		
-		// #1번
-		//start = str[0];
-		//end = str[str.length()];
-
-		// #2번
-		//start = str.at(0);
-		//end = str.at(str.length());
-
-		// #3번
-		start = str.front();
-		end = str.back();
-
-		cout << start << end << "\n";
+		cout << str.front() << str.back() << "\n";
 	}
 
 	return 0;
