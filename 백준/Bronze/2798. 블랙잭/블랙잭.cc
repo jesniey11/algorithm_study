e529640ebefb2601495a5eb8af7f4fa71a127b8e
SHUU <59950875+jesniey11@users.noreply.github.com>
2023-11-07 04:32:50 +0900

    [Bronze II] Title: 블랙잭, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Bronze/2798. 블랙잭/블랙잭.cc
+++ 백준/Bronze/2798. 블랙잭/블랙잭.cc
@@ -5,46 +5,26 @@
 using namespace std;
 
 int n, m, answer;
-bool* visit;
 int* card;
-
-void selectCard(int cnt, int now) {
-
-	if (now == 3) {
-		int sum = 0;
-		for (int i = 0; i < n; i++)
-		{
-			if (visit[i]) { sum += card[i]; }
-			if (sum > m) { sum = 0; break; }
-		}
-
-		answer = max(answer, sum);
-        return;
-	}
-
-	for (int i = cnt; i < n; i++) 
-	{
-		visit[i] = true;
-		selectCard(i + 1, now + 1);
-		visit[i] = false;
-	}
-}
 
 int main() {
 	cin >> n >> m;
-
 	card = new int[n];
-	visit = new bool[n];
-	
 	answer = 0;
 
+	for (int i = 0; i < n; i++) { cin >> card[i]; }
+    
 	for (int i = 0; i < n; i++)
 	{
-		cin >> card[i];
+		for (int j = i+1; j < n; j++) 
+		{
+			for (int k = j+1; k < n; k++)
+			{
+				int sum = card[i] + card[j] + card[k];
+				if (sum <= m && sum > answer) { answer = sum; }
+			}
+		}
 	}
-
-	selectCard(0, 0);
 	cout << answer;
-
 	return 0;
 }
