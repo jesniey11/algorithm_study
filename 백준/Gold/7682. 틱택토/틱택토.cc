39db5f32744a1c9df980a5439728e781ae01ffb1
SHUU <59950875+jesniey11@users.noreply.github.com>
2025-04-13 20:15:49 +0900

    [Gold V] Title: 틱택토, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

--- 백준/Gold/7682. 틱택토/틱택토.cc
+++ 백준/Gold/7682. 틱택토/틱택토.cc
@@ -2,14 +2,32 @@
 
 using namespace std;
 
+char board[3][3];
+
+int check(char winner)
+{
+    int wins = 0;
+
+    // 가로 / 세로
+    for(int i = 0; i < 3; i++) {
+        if(board[i][0] == winner && board[i][1] == winner && board[i][2] == winner) wins++;
+        if(board[0][i] == winner && board[1][i] == winner && board[2][i] == winner) wins++;
+    }
+    // 대각선
+    if(board[0][0] == winner && board[1][1] == winner && board[2][2] == winner) wins++;
+    if(board[0][2] == winner && board[1][1] == winner && board[2][0] == winner) wins++;
+
+    return wins;
+}
+
 int main()
 {
     while(true)
     {
-        char board[3][3];
+        int X = 0, O = 0;
         string str;
-
         cin >> str;
+
         if(str == "end") break;
 
         for(int i = 0; i < 3; i++)
@@ -17,64 +35,27 @@ int main()
             for(int j = 0; j < 3; j++) 
             {
                 board[i][j] = str.at(i*3+j);
-            }
-        }
-        
-        // X, O 개수
-        int X = 0, O = 0; 
-        for(int i = 0; i < 3; i++)
-        {
-            for(int j = 0; j < 3; j++)
-            {
+
                 if(board[i][j] == 'X') X++;
                 if(board[i][j] == 'O') O++;
             }
         }
+        
+        int winX = check('X');
+        int winO = check('O');
 
-        bool flag = true;
-        int winX = 0, winO = 0;
-
-        // 대각선
-        if(board[0][0] == board[1][1] && board[1][1] == board[2][2]) 
-        {
-            if(board[0][0] == 'X') winX++;
-            if(board[0][0] == 'O') winO++;
-        }
-
-        if(board[0][2] == board[1][1] && board[1][1] == board[2][0]) 
-        {
-            if(board[0][2] == 'X') winX++;
-            if(board[0][2] == 'O') winO++;
-        }
-
-        for(int i = 0; i < 3; i++)
-        {
-            // 가로
-            if(board[i][0] == board[i][1] && board[i][1] == board[i][2])
-            {
-                if(board[i][0] == 'X') winX++;
-                if(board[i][0] == 'O') winO++;
-            }
-
-            // 세로
-            if(board[0][i] == board[1][i] && board[1][i] == board[2][i])
-            {
-                if(board[0][i] == 'X') winX++;
-                if(board[0][i] == 'O') winO++;
-            }
-        }
+        bool isVaild = true;
 
         // 이상한 게임
-        if(X < O || O+1 < X) flag = false; // X, O 개수 이상
+        if(X < O || O+1 < X) isVaild = false; // X, O 개수 이상
 
-        if(0 < winX && 0 < winO) flag = false; // 승자가 둘
-        if(0 < winX && X != O+1) flag = false; // X승, 개수 이상
-        if(0 < winO && X != O) flag = false; // O승, 개수 이상
+        if(0 < winX && 0 < winO) isVaild = false; // 승자가 둘
+        if(0 < winX && X != O+1) isVaild = false; // X승, 개수 이상
+        if(0 < winO && X != O) isVaild = false; // O승, 개수 이상
 
-        if(winX == 0 && winO == 0 && X+O != 9) flag = false; // 무승부인데 판 다 안참
+        if(winX == 0 && winO == 0 && X+O != 9) isVaild = false; // 안 끝남
 
-        if(flag) cout << "valid" << endl;
-        else cout << "invalid" << endl;
+        cout << (isVaild ? "valid" : "invalid") << endl;
     }
 
     return 0;
