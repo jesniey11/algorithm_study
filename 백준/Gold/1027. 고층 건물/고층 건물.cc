91aca4234e15a5ac30f785052d312663f6c2816d
SHUU <59950875+jesniey11@users.noreply.github.com>
2025-04-26 15:38:51 +0900

    [Gold IV] Title: 고층 건물, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Gold/1027. 고층 건물/고층 건물.cc
+++ 백준/Gold/1027. 고층 건물/고층 건물.cc
@@ -2,55 +2,46 @@
 
 using namespace std;
 
+int N, ans = 0;
+long long bd[51];
+
+void fastio()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(0);
+    cout.tie(0);
+}
+
+bool isVisible(int i, int j)
+{
+    int s = min(i, j);
+    int e = max(i, j);
+
+    double a = (double)(bd[j]-bd[i])/(j-i);
+    double b = bd[i]-(a*i);
+
+    for(int x = s+1; x < e; x++)
+    {
+        if(bd[x] < (a*x)+b) continue;
+        else return false;
+    }
+
+    return true;
+}
+
 int main()
 {
-    int N, ans = 0;
-    long long bd[51];
+    fastio();
     cin >> N;
-
     for(int i = 1; i <= N; i++) cin >> bd[i];
 
     for(int i = 1; i <= N; i++) 
     {
-        // i = 탐색 기준점
-        int cnt = 0; // i에서 보이는 빌딩 수
-
-        for(int l = 1; l < i; l++) 
+        int cnt = 0;
+        for(int j = 1; j <= N; j++)
         {
-            // l = i의 왼쪽에 있는 빌딩
-            double a = (double)(bd[i]-bd[l])/(i-l);
-            double b = bd[l]-(a*l);
-            bool flag = true;
-
-            for(int j = l+1; j < i; j++) 
-            {
-                if((a*j)+b <= bd[j]) 
-                {
-                    flag = false;
-                    break;
-                }
-            }
-
-            if(flag) cnt++;
-        }
-
-        for(int r = i+1; r <= N; r++) 
-        {
-            // r = i의 오른쪽에 있는 빌딩
-            double a = (double)(bd[r]-bd[i])/(r-i);
-            double b = bd[r]-(a*r);
-            bool flag = true;
-
-            for(int j = i+1; j < r; j++) 
-            {
-                if((double)(a*j)+b <= bd[j]) 
-                {
-                    flag = false;
-                    break;
-                }
-            }
-
-            if(flag) cnt++;
+            if(i == j) continue;
+            if(isVisible(i, j)) cnt++;
         }
 
         ans = max(ans, cnt);
