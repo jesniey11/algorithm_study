e4781614f583311cdd343330c76456b8811418f0
SHUU <59950875+jesniey11@users.noreply.github.com>
2025-04-27 17:19:29 +0900

    [Gold IV] Title: 비슷한 단어, Time: 432 ms, Memory: 3572 KB -BaekjoonHub

---
+++ 백준/Gold/2179. 비슷한 단어/비슷한 단어.cc
@@ -0,0 +1,45 @@
+#include<iostream>
+#include<algorithm>
+#include<vector>
+
+using namespace std;
+
+int main()
+{
+    int N, M = -1;
+    string ans1, ans2;
+    vector<string> in, copy;
+    
+    cin >> N;
+    for(int i = 0; i < N; i++) 
+    {
+        string s; 
+        cin >> s;
+        in.push_back(s);
+    }
+
+    for(int i = 0; i < N; i++) 
+    {
+        for(int j = i+1; j < N; j++) 
+        {
+            int cnt = 0;
+            int size = min(in[i].size(), in[j].size());
+
+            for(int k = 0; k < size; k++)
+            {
+                if(in[i][k] == in[j][k]) cnt++;
+                else break;
+            }
+
+            if(M < cnt) 
+            {
+                ans1 = in[i];
+                ans2 = in[j];
+                M = cnt;
+            }
+        }
+    }
+
+    cout << ans1 << "\n" << ans2 << "\n";
+    return 0;
+}
