69544de3302352960d06b7898902adf2a57bd257
SHUU <59950875+jesniey11@users.noreply.github.com>
2025-04-28 19:32:27 +0900

    [Gold IV] Title: 줄세우기, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Gold/2631. 줄세우기/줄세우기.cc
+++ 백준/Gold/2631. 줄세우기/줄세우기.cc
@@ -15,15 +15,16 @@ int main()
     int N, ans = 0, kids[201], dp[201];
     
     cin >> N;
-    for(int i = 1; i <= N; i++) cin >> kids[i];
+    for(int i = 0; i < N; i++) cin >> kids[i];
     
-    for(int i = 1; i <= N; i++) 
+    for(int i = 0; i < N; i++) 
     {
         dp[i] = 1;
-        for(int j = 1; j <= i; j++) 
+        for(int j = 0; j < i; j++) 
         {
             if(kids[i] > kids[j]) dp[i] = max(dp[i], dp[j] + 1);
         }
+
         ans = max(ans, dp[i]);
     }
 
