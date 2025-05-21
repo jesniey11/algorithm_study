3bcfe9bd182ddb4e31c64e684117cbf66c012940
SHUU <59950875+jesniey11@users.noreply.github.com>
2025-05-21 18:48:58 +0900

    [Gold V] Title: 회문, Time: 12 ms, Memory: 2500 KB -BaekjoonHub

---
+++ 백준/Gold/17609. 회문/회문.cc
@@ -0,0 +1,51 @@
+#include<iostream>
+
+using namespace std;
+
+string str;
+
+void fastio()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(0);
+    cout.tie(0);
+}
+
+int palindrome(int s, int e, int del)
+{
+    while(s < e)
+    {
+        if(str[s] != str[e]) 
+        {
+            if(del == 0) 
+            {
+                if(palindrome(s+1, e, 1) == 0 || palindrome(s, e-1, 1) == 0) return 1;
+                return 2;
+            }
+            else return 2;
+        }
+        else 
+        {
+            s++; e--;
+        } 
+    }
+
+    return 0;
+}
+
+
+int main()
+{
+    fastio();
+
+    int T;
+    cin >> T;
+
+    for(int tc = 0; tc < T; tc++)
+    {
+        cin >> str;
+        cout << palindrome(0, str.size()-1, 0) << "\n";
+    }
+    
+    return 0;
+}
