bfb1c376b775d017b21bfc61f87990cc9e992909
SHUU <59950875+jesniey11@users.noreply.github.com>
2025-05-22 17:56:38 +0900

    [Gold V] Title: A와 B, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Gold/12904. A와 B/A와 B.cc
@@ -0,0 +1,22 @@
+#include <iostream>
+#include<algorithm>
+using namespace std;
+int main() {
+	string S, T;
+	cin >> S >> T;
+	while (1) {
+		if (T[T.size() - 1] == 'A') {
+			T.pop_back();
+		}
+		else {
+			T.pop_back();
+			reverse(T.begin(), T.end());
+		}
+		if (T.size() == S.size()) {
+			if (T == S) cout << 1;
+			else cout << 0;
+			break;
+		}
+	}
+	return 0;
+}
