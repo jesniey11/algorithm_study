4bd48cc1d861e70486b7b22b8428fac3b69b54bf
SHUU <59950875+jesniey11@users.noreply.github.com>
2025-05-25 22:56:41 +0900

    [Gold IV] Title: 수열의 점수, Time: 16 ms, Memory: 3688 KB -BaekjoonHub

--- 백준/Gold/2036. 수열의 점수/수열의 점수.cc
+++ 백준/Gold/2036. 수열의 점수/수열의 점수.cc
@@ -4,14 +4,23 @@
 
 using namespace std;
 
+void fastio()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(0);
+    cout.tie(0);
+}
+
 int main()
 {
-    long long N;
+    fastio();
+    
+    int N;
     long long ans = 0;
     vector<long long> v;
 
     cin >> N;
-    for(long long i = 0; i < N; i++) 
+    for(int i = 0; i < N; i++) 
     {
         long long n; cin >> n;
         v.push_back(n);
@@ -20,30 +29,36 @@ int main()
     sort(v.begin(), v.end());
 
     int i = 0;
-    while(i + 1 < N && v[i] <= 0) 
+    while(i+1 < N && v[i] <= 0) 
     {
-        if(v[i] != 0 && v[i+1] <= 0)
+        if(v[i+1] <= 0)
         {
             ans += v[i] * v[i+1];
+            i += 2;
+        }
+        else
+        {
+            ans += v[i];
             i++;
         }
-        else ans += v[i];
-        i++;
     }
     if (i < N && v[i] < 0) ans += v[i];
 
-    i = N-1;
-    while(i > 0 && v[i] > 0) 
+    int j = N-1;
+    while(j > 0 && v[j] > 0) 
     {
-        if(v[i] > 0 && v[i]+v[i-1] < v[i]*v[i-1])
+        if(v[j-1] > 1)
         {
-            ans += v[i] * v[i-1];
-            i--;
+            ans += v[j] * v[j-1];
+            j -= 2;
         }
-        else ans += v[i];
-        i--;
+        else
+        {
+            ans += v[j];
+            j--;
+        }
     }
-    if (i == 0 && v[i] > 0) ans += v[i];
+    if (j == 0 && v[j] > 0) ans += v[j];
 
     cout << ans;
     return 0;
