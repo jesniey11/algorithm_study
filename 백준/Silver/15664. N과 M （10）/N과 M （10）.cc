3822fd3dc16fce85db2f7ba745154bb1a009d433
SHUU <59950875+jesniey11@users.noreply.github.com>
2025-07-04 16:25:50 +0900

    [Silver II] Title: N과 M (10), Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Silver/15664. N과 M （10）/N과 M （10）.cc
@@ -0,0 +1,47 @@
+#include<iostream>
+#include<algorithm>
+
+using namespace std;
+
+int N, M;
+int arr[9], ans[9];
+
+void fastio()
+{
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+}
+
+void solve(int n, int next)
+{
+    if(n == M) 
+    {
+        for(int i = 0; i < M; i++) cout << ans[i] << " ";
+        cout << "\n";
+
+        return;
+    }
+
+    int tmp = 0;
+    for(int i = next; i < N; i++) 
+    {
+        if(tmp == arr[i]) continue;
+        
+        tmp = ans[n] = arr[i];
+        solve(n+1, i+1);
+    }
+}
+
+int main()
+{
+    fastio();
+
+    cin >> N >> M;
+    for(int i = 0; i < N; i++) cin >> arr[i];
+
+    sort(arr, arr+N);
+    solve(0, 0);
+
+    return 0;
+}
