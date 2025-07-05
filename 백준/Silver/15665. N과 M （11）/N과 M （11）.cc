6d27042319c64bd7989a7aac4d9bd862ccf58e81
SHUU <59950875+jesniey11@users.noreply.github.com>
2025-07-05 19:32:47 +0900

    [Silver II] Title: N과 M (11), Time: 140 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Silver/15665. N과 M （11）/N과 M （11）.cc
@@ -0,0 +1,38 @@
+#include<iostream>
+#include<algorithm>
+
+using namespace std;
+
+int N, M;
+int arr[9], ans[9];
+
+void solve(int n)
+{
+    if(n == M) 
+    {
+        for(int i = 0; i < M; i++) cout << ans[i] << " ";
+        cout << "\n";
+
+        return;
+    }
+
+    int tmp = 0;
+    for(int i = 0; i < N; i++) 
+    {
+        if(tmp == arr[i]) continue;
+        
+        tmp = ans[n] = arr[i];
+        solve(n+1);
+    }
+}
+
+int main()
+{
+    cin >> N >> M;
+    for(int i = 0; i < N; i++) cin >> arr[i];
+
+    sort(arr, arr+N);
+    solve(0);
+
+    return 0;
+}
