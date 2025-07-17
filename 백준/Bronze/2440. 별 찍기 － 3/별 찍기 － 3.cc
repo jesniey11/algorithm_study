3288f79bbba9f3fceafae20c5f33e33664a1354d
SHUU <59950875+jesniey11@users.noreply.github.com>
2025-07-18 01:31:31 +0900

    [Bronze IV] Title: 별 찍기 - 3, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/2440. 별 찍기 － 3/별 찍기 － 3.cc
@@ -0,0 +1,17 @@
+#include<iostream>
+
+using namespace std;
+
+int main()
+{
+    int N;
+    cin >> N;
+
+    for(int i = N; i >= 0; i--) 
+    {
+        for(int j = 0; j < i; j++) cout << '*';
+        cout << "\n";
+    }
+    
+    return 0;
+}
