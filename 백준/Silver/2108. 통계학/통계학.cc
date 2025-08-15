29903d7acb1e725fc1597ca76190fabb03fbe525
SHUU <59950875+jesniey11@users.noreply.github.com>
2025-08-15 17:08:05 +0900

    [Silver II] Title: 통계학, Time: 68 ms, Memory: 4004 KB -BaekjoonHub

--- 백준/Silver/2108. 통계학/통계학.cc
+++ 백준/Silver/2108. 통계학/통계학.cc
@@ -3,9 +3,8 @@
 
 using namespace std;
 
-int N, x;
-int sum, mean, freq, cnt, mode, mn = 9999, mx = -9999;
-int arr[500001], med[500001], num[8005];
+int N, sum;
+int arr[500001], num[8005];
 
 void fastio()
 {
@@ -14,49 +13,58 @@ void fastio()
     cout.tie(0);
 }
 
+int average()
+{
+    if(sum < 0) return (sum - N/2) / N;
+    else return (sum + N/2) / N;
+}
+
+int median()
+{
+    sort(arr, arr+N);
+    return arr[N/2];
+}
+
+int mode()
+{
+    int freq = 0, cnt = 0, res = 0;
+
+    for (int i = 0; i < 8001; i++) freq = max(freq, num[i]);
+    for (int i = 0; i < 8001; i++) 
+    {
+        if (num[i] == freq) 
+        {
+            int now = i - 4000; cnt++;
+            
+            if (cnt == 1) res = now; 
+            else if (cnt == 2) return now;
+        }
+    }
+    
+    return res;
+}
+
+int range()
+{
+    return arr[N-1] - arr[0];
+}
+
 int main()
 {
     fastio();
 
     cin >> N;
-    for(int i = 0; i < N; i++) 
+    for (int i = 0; i < N; i++) 
     {
-        cin >> x;
-        
-        arr[i] = med[i] = x;
-        sum += x;
-        num[x+4000]++;
+        cin >> arr[i];
+        sum += arr[i];
+        num[arr[i] + 4000]++;
     }
 
-    // 산술평균
-    if(sum < 0) mean = (sum - N/2) / N;
-    else mean = (sum + N/2) / N;
-    cout << mean << "\n";
-
-    // 중앙값
-    sort(med, med+N);
-    cout << med[N/2] << "\n";
-
-    // 최빈값
-    for(int i = 0; i < N; i++) freq = max(freq, num[arr[i]+4000]);
-    for(int i = 0; i < N; i++)
-    {
-        if(freq == num[med[i]+4000] && mode != med[i])
-        {
-            cnt++; 
-            mode = med[i];
-        }
-        if(cnt == 2) break;
-    }
-    cout << mode << "\n";
-
-    // 범위
-    for(int i = 0; i < N; i++)
-    {
-        mn = min(mn, arr[i]);
-        mx = max(mx, arr[i]);
-    }
-    cout << mx-mn;
+    cout << average() << '\n'; // 산술평균
+    cout << median() << '\n'; // 중앙값
+    cout << mode() << '\n'; // 최빈값
+    cout << range(); // 범위
 
     return 0;
 }
