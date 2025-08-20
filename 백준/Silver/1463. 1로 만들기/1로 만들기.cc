edded73dafcb6c41585dd54dc6f8c253deb619dd
SHUU <59950875+jesniey11@users.noreply.github.com>
2025-08-20 21:53:51 +0900

    [Silver III] Title: 1로 만들기, Time: 0 ms, Memory: 2996 KB -BaekjoonHub

--- 백준/Silver/1463. 1로 만들기/1로 만들기.cc
+++ 백준/Silver/1463. 1로 만들기/1로 만들기.cc
@@ -6,54 +6,48 @@
 
 using namespace std;
 
-int N, m = 999999999;
+int N;
 queue<pair<int, int>> q;
 bool visit[MAX];
 
-void BFS()
-{
+int BFS()
+{    
     q.push({N, 0});
     visit[N] = true;
-    
+
     while(!q.empty())
     {
-        pair<int, int> p = q.front(); q.pop();
+        int x = q.front().first;
+        int d = q.front().second;
+        q.pop();
 
-        if(p.first == 1) 
+        if(x == 1) return d;
+
+        if(x%3 == 0 && !visit[x/3]) 
         {
-            m = min(m, p.second);
-            return;
+            q.push({x/3, d+1});
+            visit[x/3] = true;
         }
 
-        for(int i = 0; i < 3; i++)
+        if(x % 2 == 0 && !visit[x/2])
         {
-            int nx = p.first;
+            q.push({x/2, d+1});
+            visit[x/2] = true;
+        }
 
-            switch (i)
-            {
-                case 0: 
-                    if(nx % 3 == 0) nx /= 3; break;
-
-                case 1: 
-                    if(nx % 2 == 0) nx /= 2; break;
-
-                case 2: 
-                    nx--; break;
-            }
-
-            if(nx < 1 || MAX <= nx || visit[nx]) continue;
-            
-            q.push({nx, p.second+1});
-            visit[nx] = true;
+        if (x-1 > 0 && !visit[x-1]) {
+            q.push({x-1, d+1});
+            visit[x-1] = true;
         }
     }
+
+    return -1;
 }
 
 int main()
 {
     cin >> N;
-    BFS();
+    cout << BFS();
 
-    cout << m;
     return 0;
 }
