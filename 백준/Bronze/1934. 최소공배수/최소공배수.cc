9e4f0bca12dd29f02ffeea9ee87f888e1c50e259
SHUU <59950875+jesniey11@users.noreply.github.com>
2025-08-21 23:14:34 +0900

    [Bronze I] Title: 최소공배수, Time: 28 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Bronze/1934. 최소공배수/최소공배수.cc
+++ 백준/Bronze/1934. 최소공배수/최소공배수.cc
@@ -4,16 +4,22 @@ using namespace std;
 
 int tc, A, B, ans;
 
+int gcd(int a, int b)
+{
+    int r = a % b;
+
+    if(r == 0) return b;
+    else return gcd(b, r);
+}
+
 int main()
 {
     cin >> tc;
     while(tc--)
     {
         cin >> A >> B;
-
-        ans = A;
-        while(ans%B != 0) ans += A;
-
+        ans = (A*B) / gcd(A, B);
+        
         cout << ans << '\n';
     }
 
