7787f7f488bf92cd4a88f5c5650f37d670ee59ae
SHUU <59950875+jesniey11@users.noreply.github.com>
2025-08-27 19:12:24 +0900

    [Silver II] Title: 가장 큰 증가하는 부분 수열, Time: 0 ms, Memory: 2028 KB -BaekjoonHub

---
+++ 백준/Silver/11055. 가장 큰 증가하는 부분 수열/가장 큰 증가하는 부분 수열.cc
@@ -0,0 +1,36 @@
+#include<iostream>
+
+using namespace std;
+
+int N, ans = 0;
+int arr[1001], dp[1001];
+
+void fastin()
+{
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+}
+
+int main()
+{
+    fastin();
+
+    cin >> N;
+    for(int i = 0; i < N; i++) 
+    {
+        cin >> arr[i];
+        dp[i] = arr[i];
+    }
+
+    for(int i = 0; i < N; i++)
+    {
+        for(int j = 0; j < i; j++) 
+        {
+            if(arr[i] > arr[j]) dp[i] = max(dp[i], arr[i]+dp[j]);
+        }
+    }
+    for(int i = 0; i < N; i++) ans = max(ans, dp[i]);
+
+    cout << ans;
+    return 0;
+}
